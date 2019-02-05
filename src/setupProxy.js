f0ad91e24d22fe2b0a80f5c714d9d8d038bed361
Volodymyr Kryvko <kryvkovr@gmail.com>
2019-02-05 19:18:10 +0200

    finish configuring proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
-  app.use(proxy('/api/*', {
-    target: 'http://localhost:8080/',
+  app.use(proxy('/api', {
+    target: process.env.REACT_APP_API_URL,
     changeOrigin: true,
     autoRewrite: true,
     cookieDomainRewrite: '',
